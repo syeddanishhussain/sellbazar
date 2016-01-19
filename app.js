38d44bbaa034ec5c91f3b302ebbd7e0856771a32
syeddanishhussain <syeddanish_hussain@hotmail.com>
2016-01-19 22:24:55 +0500

    commit

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /**
  * Created by dell1 on 13/01/2016.
  */
-angular.module("sellbazarApp",["ngMaterial","ngMdIcons"])
+angular.module("sellbazarApp",["ngMaterial","ngMdIcons","firebase"])
+var Firebase = require("firebase");
+var myFirebaseRef = new Firebase("https://sellbazar.firebaseio.com/");
