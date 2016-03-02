d76334284f00f0c97fd8d2328fe87945b27d9d94
syeddanishhussain <syeddanish_hussain@hotmail.com>
2016-03-02 21:43:06 +0500

    sadsa

--- public/app.js
+++ public/app.js
@@ -1,13 +1,14 @@
 /**
  * Created by dell1 on 13/01/2016.
  */
-angular.module("sellbazarApp",["ngMaterial","ngMdIcons"])
+angular.module("sellbazarApp", ["ngMaterial", "ngMdIcons"])
+
 //var Firebase = require("firebase");
 //var myFirebaseRef = new Firebase("https://sellbazar.firebaseio.com/");
-    .controller("applicationController",function($scope, $mdDialog, $mdMedia){
+    .controller("applicationController", function ($scope, $mdDialog, $mdMedia, $timeout, $mdSidenav, $log) {
         $scope.status = '  ';
         $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
-        $scope.showAlert = function(ev) {
+        $scope.showAlert = function (ev) {
             // Appending dialog to document.body to cover sidenav in docs app
             // Modal dialogs should fully cover application
             // to prevent interaction outside of dialog
@@ -22,7 +23,7 @@ angular.module("sellbazarApp",["ngMaterial","ngMdIcons"])
                     .targetEvent(ev)
             );
         };
-        $scope.showConfirm = function(ev) {
+        $scope.showConfirm = function (ev) {
             // Appending dialog to document.body to cover sidenav in docs app
             var confirm = $mdDialog.confirm()
                 .title('Would you like to delete your debt?')
@@ -31,241 +32,320 @@ angular.module("sellbazarApp",["ngMaterial","ngMdIcons"])
                 .targetEvent(ev)
                 .ok('Please do it!')
                 .cancel('Sounds like a scam');
-            $mdDialog.show(confirm).then(function() {
+            $mdDialog.show(confirm).then(function () {
                 $scope.status = 'You decided to get rid of your debt.';
-            }, function() {
+            }, function () {
                 $scope.status = 'You decided to keep your debt.';
             });
         };
-        $scope.showAdvanced = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.showAdvanced = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
             $mdDialog.show({
                 controller: DialogController,
                 templateUrl: 'dialog1.tmpl.html',
                 parent: angular.element(document.body),
                 targetEvent: ev,
-                clickOutsideToClose:true,
+                clickOutsideToClose: true,
                 fullscreen: useFullScreen
             })
-                .then(function(answer) {
+                .then(function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
-            $scope.$watch(function() {
+            $scope.$watch(function () {
                 return $mdMedia('xs') || $mdMedia('sm');
-            }, function(wantsFullScreen) {
+            }, function (wantsFullScreen) {
                 $scope.customFullscreen = (wantsFullScreen === true);
             });
         };
-        $scope.showAdvanced1 = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.showAdvanced1 = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
             $mdDialog.show({
                 controller: DialogController,
                 templateUrl: 'dialog2.tmpl.html',
                 parent: angular.element(document.body),
                 targetEvent: ev,
-                clickOutsideToClose:true,
+                clickOutsideToClose: true,
                 fullscreen: useFullScreen
             })
-                .then(function(answer) {
+                .then(function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
-            $scope.$watch(function() {
+            $scope.$watch(function () {
                 return $mdMedia('xs') || $mdMedia('sm');
-            }, function(wantsFullScreen) {
+            }, function (wantsFullScreen) {
                 $scope.customFullscreen = (wantsFullScreen === true);
             });
         };
 
 
-        $scope.showAdvanced2 = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.showAdvanced2 = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
             $mdDialog.show({
-                    controller: DialogController,
-                    templateUrl: 'dialog3.tmpl.html',
-                    parent: angular.element(document.body),
-                    targetEvent: ev,
-                    clickOutsideToClose:true,
-                    fullscreen: useFullScreen
-                })
-                .then(function(answer) {
+                controller: DialogController,
+                templateUrl: 'dialog3.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            })
+                .then(function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
-            $scope.$watch(function() {
+            $scope.$watch(function () {
                 return $mdMedia('xs') || $mdMedia('sm');
-            }, function(wantsFullScreen) {
+            }, function (wantsFullScreen) {
                 $scope.customFullscreen = (wantsFullScreen === true);
             });
         };
 
 
-        $scope.showAdvanced3 = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.showAdvanced3 = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
             $mdDialog.show({
-                    controller: DialogController,
-                    templateUrl: 'dialog4.tmpl.html',
-                    parent: angular.element(document.body),
-                    targetEvent: ev,
-                    clickOutsideToClose:true,
-                    fullscreen: useFullScreen
-                })
-                .then(function(answer) {
+                controller: DialogController,
+                templateUrl: 'dialog4.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            })
+                .then(function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
-            $scope.$watch(function() {
+            $scope.$watch(function () {
                 return $mdMedia('xs') || $mdMedia('sm');
-            }, function(wantsFullScreen) {
+            }, function (wantsFullScreen) {
                 $scope.customFullscreen = (wantsFullScreen === true);
             });
         };
 
 
-        $scope.showAdvanced4 = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.showAdvanced4 = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
             $mdDialog.show({
-                    controller: DialogController,
-                    templateUrl: 'dialog5.tmpl.html',
-                    parent: angular.element(document.body),
-                    targetEvent: ev,
-                    clickOutsideToClose:true,
-                    fullscreen: useFullScreen
-                })
-                .then(function(answer) {
+                controller: DialogController,
+                templateUrl: 'dialog5.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            })
+                .then(function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
-            $scope.$watch(function() {
+            $scope.$watch(function () {
                 return $mdMedia('xs') || $mdMedia('sm');
-            }, function(wantsFullScreen) {
+            }, function (wantsFullScreen) {
                 $scope.customFullscreen = (wantsFullScreen === true);
             });
         };
 
 
-
-        $scope.showAdvanced5 = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.showAdvanced5 = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
             $mdDialog.show({
-                    controller: DialogController,
-                    templateUrl: 'dialog6.tmpl.html',
-                    parent: angular.element(document.body),
-                    targetEvent: ev,
-                    clickOutsideToClose:true,
-                    fullscreen: useFullScreen
-                })
-                .then(function(answer) {
+                controller: DialogController,
+                templateUrl: 'dialog6.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            })
+                .then(function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
-            $scope.$watch(function() {
+            $scope.$watch(function () {
                 return $mdMedia('xs') || $mdMedia('sm');
-            }, function(wantsFullScreen) {
+            }, function (wantsFullScreen) {
                 $scope.customFullscreen = (wantsFullScreen === true);
             });
         };
 
 
-
-        $scope.showAdvanced6 = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.showAdvanced6 = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
             $mdDialog.show({
-                    controller: DialogController,
-                    templateUrl: 'dialog7.tmpl.html',
-                    parent: angular.element(document.body),
-                    targetEvent: ev,
-                    clickOutsideToClose:true,
-                    fullscreen: useFullScreen
-                })
-                .then(function(answer) {
+                controller: DialogController,
+                templateUrl: 'dialog7.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            })
+                .then(function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
-            $scope.$watch(function() {
+            $scope.$watch(function () {
                 return $mdMedia('xs') || $mdMedia('sm');
-            }, function(wantsFullScreen) {
+            }, function (wantsFullScreen) {
                 $scope.customFullscreen = (wantsFullScreen === true);
             });
         };
 
 
-
-        $scope.showAdvanced7 = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.showAdvanced7 = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
             $mdDialog.show({
-                    controller: DialogController,
-                    templateUrl: 'dialog8.tmpl.html',
-                    parent: angular.element(document.body),
-                    targetEvent: ev,
-                    clickOutsideToClose:true,
-                    fullscreen: useFullScreen
-                })
-                .then(function(answer) {
+                controller: DialogController,
+                templateUrl: 'dialog8.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            })
+                .then(function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
-            $scope.$watch(function() {
+            $scope.$watch(function () {
                 return $mdMedia('xs') || $mdMedia('sm');
-            }, function(wantsFullScreen) {
+            }, function (wantsFullScreen) {
                 $scope.customFullscreen = (wantsFullScreen === true);
             });
         };
 
 
-
-        $scope.showAdvanced8 = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.showAdvanced8 = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
             $mdDialog.show({
-                    controller: DialogController,
-                    templateUrl: 'dialog9.tmpl.html',
-                    parent: angular.element(document.body),
-                    targetEvent: ev,
-                    clickOutsideToClose:true,
-                    fullscreen: useFullScreen
-                })
-                .then(function(answer) {
+                controller: DialogController,
+                templateUrl: 'dialog9.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            })
+                .then(function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
-            $scope.$watch(function() {
+            $scope.$watch(function () {
                 return $mdMedia('xs') || $mdMedia('sm');
-            }, function(wantsFullScreen) {
+            }, function (wantsFullScreen) {
                 $scope.customFullscreen = (wantsFullScreen === true);
             });
         };
 
 
-
-
-
-
-
-        $scope.showTabDialog = function(ev) {
+        $scope.showTabDialog = function (ev) {
             $mdDialog.show({
                 controller: DialogController,
                 templateUrl: 'tabDialog.tmpl.html',
                 parent: angular.element(document.body),
                 targetEvent: ev,
-                clickOutsideToClose:true
+                clickOutsideToClose: true
             })
-                .then(function(answer) {
+                .then(function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
-                }, function() {
+                }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
         };
+        ;
+
+//chat area
+        $scope.showAdvanced22 = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'chat.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            })
+                .then(function (answer) {
+                    $scope.status = 'You said the information was "' + answer + '".';
+                }, function () {
+                    $scope.status = 'You cancelled the dialog.';
+                });
+            $scope.$watch(function () {
+                return $mdMedia('xs') || $mdMedia('sm');
+            }, function (wantsFullScreen) {
+                $scope.customFullscreen = (wantsFullScreen === true);
+            });
+        };
+        $scope.toggleLeft = buildDelayedToggler('left');
+        $scope.toggleRight = buildToggler('right');
+        $scope.isOpenRight = function () {
+            return $mdSidenav('right').isOpen();
+        };
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        function debounce(func, wait, context) {
+            var timer;
+            return function debounced() {
+                var context = $scope,
+                    args = Array.prototype.slice.call(arguments);
+                $timeout.cancel(timer);
+                timer = $timeout(function () {
+                    timer = undefined;
+                    func.apply(context, args);
+                }, wait || 10);
+            };
+        }
+
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildDelayedToggler(navID) {
+            return debounce(function () {
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }, 200);
+        }
+
+        function buildToggler(navID) {
+            return function () {
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            }
+        }
+    })
+    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+            $mdSidenav('left').close()
+                .then(function () {
+                    $log.debug("close LEFT is done");
+                });
+        };
+    })
+    .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+            $mdSidenav('right').close()
+                .then(function () {
+                    $log.debug("close RIGHT is done");
+                });
+        };
+
     });
+
+
 function DialogController($scope, $mdDialog) {
     $scope.hide = function () {
         $mdDialog.hide();
